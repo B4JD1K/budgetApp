4bc2fb57e34f6a83e211a841737007d8dd489820
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2024-05-03 20:24:24 +0200

    created BudgetController, checking is app with mapping properly configured

---
+++ budget-app-backend/src/main/java/com/budget/application/controller/BudgetController.java
@@ -0,0 +1,14 @@
+package com.budget.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BudgetController {
+
+    @RequestMapping(value ="/helloworld",method = RequestMethod.GET)
+    public String sayHelloWorld(){
+        return "Hello World";
+    }
+}
