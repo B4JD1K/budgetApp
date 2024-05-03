301c3d5cd3d0db221561652583bebd7bd4ea18c9
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2024-05-04 00:43:31 +0200

    added tag and expense JPA repositories

---
+++ budget-app-backend/src/main/java/com/budget/application/repository/ExpenseRepository.java
@@ -0,0 +1,16 @@
+package com.budget.application.repository;
+
+import com.budget.application.model.Expense;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ExpenseRepository {
+    Optional<Expense> findById(Long id);
+
+    Expense save(Expense expense);
+
+    List<Expense> findAll();
+}
