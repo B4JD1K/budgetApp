2164c9555b28fbba5d027e3c98368b41b9fda249
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2024-05-03 20:25:41 +0200

    created models for expenses and tags. added getters and setters

---
+++ budget-app-backend/src/main/java/com/budget/application/model/Tag.java
@@ -0,0 +1,52 @@
+package com.budget.application.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @Column(unique = true, length = 30)
+    private String name;
+
+    @ManyToMany(mappedBy = "tags", fetch = FetchType.EAGER)
+    private List<Expense> expenses = new ArrayList<Expense>();
+
+    public Tag() {
+    }
+
+    public Tag(String name) {
+        super();
+        this.name = name;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(List<Expense> expenses) {
+        this.expenses = expenses;
+    }
+}
