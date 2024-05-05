6aed722da258715213ff9646a2ef576600cbce25
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2024-05-05 14:00:48 +0200

    corrected ExpenseRepository by extending with CrudRepository<T, ID>, that was necessary to create JUnit Tests. deleted unnecessary 'void deleteById' cuz it's implemented in CrudRepository.

--- budget-app-backend/src/main/java/com/budget/application/repository/ExpenseRepository.java
+++ budget-app-backend/src/main/java/com/budget/application/repository/ExpenseRepository.java
@@ -1,18 +1,17 @@
 package com.budget.application.repository;
 
 import com.budget.application.model.Expense;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ExpenseRepository {
+public interface ExpenseRepository extends CrudRepository<Expense, Long> {
     Optional<Expense> findById(Long id);
 
     Expense save(Expense expense);
 
     List<Expense> findAll();
-
-    void deleteById(Long expenseId);
 }
