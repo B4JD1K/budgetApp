47b69a1d9905936b6cdd793f37ffc1cb56265f99
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2024-07-22 18:28:06 +0200

    created template for BudgetController with firstly initialized "Object" that will be changed for proper Entity.

--- budget-app-backend/src/main/java/com/budget/application/controller/BudgetController.java
+++ budget-app-backend/src/main/java/com/budget/application/controller/BudgetController.java
@@ -1,11 +1,6 @@
 package com.budget.application.controller;
 
 import com.budget.application.model.Expense;
-import com.budget.application.response.provider.ExpenseResponseEntity;
-import com.budget.application.response.provider.ExpenseResponseProvider;
-import com.budget.application.response.provider.TagResponseEntity;
-import com.budget.application.response.provider.TagResponseProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,46 +8,50 @@ import java.util.List;
 @RestController
 public class BudgetController {
 
-    @Autowired
-    private ExpenseResponseProvider expenseResponseProvider;
 
-    @Autowired
-    private TagResponseProvider tagResponseProvider;
-
+    //ExpensesProvider
+    //TagsProvider
     @RequestMapping(value = "/expense", method = RequestMethod.POST)
-    public ExpenseResponseEntity addExpense(@RequestBody Expense expense) {
-        return expenseResponseProvider.createExpense(expense);
+    public Object addExpense(@RequestBody Expense expense) {
+
+        return null;
     }
 
     @RequestMapping(value = "/expense/{expenseId}", method = RequestMethod.DELETE)
-    public ExpenseResponseEntity deleteExpense(@PathVariable(value = "expenseId") Long expenseId) {
-        return expenseResponseProvider.deleteExpense(expenseId);
+    public Object deleteExpense(@PathVariable(value = "expenseId") Long expenseId) {
+
+        return null;
     }
 
     @RequestMapping(value = "/expenses", method = RequestMethod.GET)
-    public ExpenseResponseEntity getAllExpenses() {
-        return expenseResponseProvider.getAllExpenses();
+    public Object getAllExpenses() {
+
+        return null;
     }
 
     @RequestMapping(value = "/expense/criteria", method = RequestMethod.GET)
-    public ExpenseResponseEntity getExpensesBySearchCriteria(@RequestParam(value = "tagNames") List<String> tagNames,
-                                                             @RequestParam(value = "fromDate") String fromDate,
-                                                             @RequestParam(value = "toDate") String toomDate) {
-        return expenseResponseProvider.getExpensesBySearchCriteria(tagNames, fromDate, toomDate);
+    public Object getExpensesBySearchCriteria(@RequestParam(value = "tagNames") List<String> tagNames,
+                                              @RequestParam(value = "fromDate") String fromDate,
+                                              @RequestParam(value = "toDate") String toomDate) {
+
+        return null;
     }
 
     @RequestMapping(value = "tags", method = RequestMethod.GET)
-    public TagResponseEntity getAllTags() {
-        return tagResponseProvider.getAllTags();
+    public Object getAllTags() {
+
+        return null;
     }
 
-    @RequestMapping(value = "tag", method = RequestMethod.POST)
-    public TagResponseEntity addNewTag(@RequestBody String tagName) {
-        return tagResponseProvider.createTag(tagName);
+    @RequestMapping(value = "tag",method = RequestMethod.POST)
+    public Object addNewTag(@RequestBody String name){
+
+        return null;
     }
 
     @RequestMapping(value = "tag/{tagId}", method = RequestMethod.DELETE)
-    public TagResponseEntity deleteTag(@PathVariable("tagId") Long tagId) {
-        return tagResponseProvider.deleteTag(tagId);
+    public Object deleteTag(@PathVariable("tagId") Long tagId){
+
+        return null;
     }
 }
