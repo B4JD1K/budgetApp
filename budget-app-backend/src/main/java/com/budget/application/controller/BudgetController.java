56841362e47a93e1a31fa653924463cb89fb7a1e
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2024-07-22 18:28:06 +0200

    created template for BudgetController with firstly initialized "Object" that will be changed for proper Entity.

--- budget-app-backend/src/main/java/com/budget/application/controller/BudgetController.java
+++ budget-app-backend/src/main/java/com/budget/application/controller/BudgetController.java
@@ -1,14 +1,57 @@
 package com.budget.application.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.budget.application.model.Expense;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BudgetController {
 
-    @RequestMapping(value ="/helloworld",method = RequestMethod.GET)
-    public String sayHelloWorld(){
-        return "Hello World";
+
+    //ExpensesProvider
+    //TagsProvider
+    @RequestMapping(value = "/expense", method = RequestMethod.POST)
+    public Object addExpense(@RequestBody Expense expense) {
+
+        return null;
+    }
+
+    @RequestMapping(value = "/expense/{expenseId}", method = RequestMethod.DELETE)
+    public Object deleteExpense(@PathVariable(value = "expenseId") Long expenseId) {
+
+        return null;
+    }
+
+    @RequestMapping(value = "/expenses", method = RequestMethod.GET)
+    public Object getAllExpenses() {
+
+        return null;
+    }
+
+    @RequestMapping(value = "/expense/criteria", method = RequestMethod.GET)
+    public Object getExpensesBySearchCriteria(@RequestParam(value = "tagNames") List<String> tagNames,
+                                              @RequestParam(value = "fromDate") String fromDate,
+                                              @RequestParam(value = "toDate") String toomDate) {
+
+        return null;
+    }
+
+    @RequestMapping(value = "tags", method = RequestMethod.GET)
+    public Object getAllTags() {
+
+        return null;
+    }
+
+    @RequestMapping(value = "tag",method = RequestMethod.POST)
+    public Object addNewTag(@RequestBody String name){
+
+        return null;
+    }
+
+    @RequestMapping(value = "tag/{tagId}", method = RequestMethod.DELETE)
+    public Object deleteTag(@PathVariable("tagId") Long tagId){
+
+        return null;
     }
 }
