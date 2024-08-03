5536f7a639aa6f4abe97bc842f7e086975fe08c0
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2024-08-03 13:51:41 +0200

    created BudgetControllerTestWithSpringRunner.java for tests, created two tests

--- budget-app-backend/src/test/java/com/budget/application/controller/BudgetControllerTestWithSpringRunner.java
+++ budget-app-backend/src/test/java/com/budget/application/controller/BudgetControllerTestWithSpringRunner.java
@@ -4,10 +4,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import com.budget.application.Application;
-import com.budget.application.model.Tag;
-import com.budget.application.response.provider.TagsList;
 import org.junit.Test;
-import org.junit.jupiter.api.Tags;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -47,7 +44,7 @@ public class BudgetControllerTestWithSpringRunner {
         ResponseEntity<ExpensesList> response = restTemplate.exchange(createURLWithPort("/expense"), HttpMethod.POST, entity, ExpensesList.class);
 
         assertTrue(response.getStatusCode().equals(HttpStatus.OK));
-        assertTrue(response.getBody().getExpenses().size() > 0);
+        assertTrue(response.getBody().getExpenses().size()>0);
     }
 
     @Test
@@ -60,72 +57,32 @@ public class BudgetControllerTestWithSpringRunner {
         ResponseEntity<ExpensesList> response = restTemplate.exchange(createURLWithPort("/expenses"), HttpMethod.GET, entity, ExpensesList.class);
 
         assertTrue(response.getStatusCode().equals(HttpStatus.OK));
-        assertTrue(response.getBody().getExpenses().size() > 0);
+        assertTrue(response.getBody().getExpenses().size()>0);
     }
 
     @Test
     public void testAddNewTag() {
-        Tag generatedTag = testUtils.generateTestTags(1, false).get(0);
-        HttpEntity<String> entity = new HttpEntity<String>(generatedTag.getName(), headers);
-        ResponseEntity<TagsList> response = restTemplate.exchange(createURLWithPort("/tag"), HttpMethod.POST, entity, TagsList.class);
-
-        assertTrue(response.getStatusCode().equals(HttpStatus.CREATED));
-        assertTrue(response.getBody().getTags().size() > 0);
+        fail("Not implemented yet.");
     }
 
     @Test
     public void testDeleteTag() {
-        Tag generatedTag = testUtils.generateTestTags(1, false).get(0);
-        HttpEntity<String> generatedTagEntity = new HttpEntity<String>(generatedTag.getName(), headers);
-        ResponseEntity<TagsList> generatedTagResponse = restTemplate.exchange(createURLWithPort("/tag"), HttpMethod.POST, generatedTagEntity, TagsList.class);
-
-        Long generatedTagId = generatedTagResponse.getBody().getTags().get(0).getId();
-        ResponseEntity<TagsList> deleteTagResponse = restTemplate.exchange(createURLWithPort("/tag/" + generatedTagId), HttpMethod.DELETE, generatedTagEntity, TagsList.class);
-
-        assertTrue(deleteTagResponse.getStatusCode().equals(HttpStatus.OK));
-
+        fail("Not implemented yet.");
     }
 
     @Test
     public void testDeleteExpense() {
-        Expense generatedExpense = testUtils.generateTestExpense(1, null);
-        HttpEntity<Expense> generatedExpenseEntity = new HttpEntity<Expense>(generatedExpense, headers);
-        ResponseEntity<ExpensesList> generatedExpenseResponse = restTemplate.exchange(createURLWithPort("/expense"), HttpMethod.POST, generatedExpenseEntity, ExpensesList.class);
-
-        Long generatedExpenseId = generatedExpenseResponse.getBody().getExpenses().get(0).getId();
-        ResponseEntity<ExpensesList> deleteExpenseResponse = restTemplate.exchange(createURLWithPort("/expense/" + generatedExpenseId), HttpMethod.DELETE, generatedExpenseEntity, ExpensesList.class);
-
-        assertTrue(deleteExpenseResponse.getStatusCode().equals(HttpStatus.OK));
+        fail("Not implemented yet.");
     }
 
     @Test
     public void testGetTags() {
-        Tag generatedTag = testUtils.generateTestTags(1, false).get(0);
-        HttpEntity<Tag> generatedTagEntity = new HttpEntity<Tag>(generatedTag, headers);
-        ResponseEntity<TagsList> generatedTagResponse = restTemplate.exchange(createURLWithPort("/tag"), HttpMethod.POST, generatedTagEntity, TagsList.class);
+        fail("Not implemented yet.");
 
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        ResponseEntity<TagsList> response = restTemplate.exchange(createURLWithPort("/tags"), HttpMethod.GET, entity, TagsList.class);
-        TagsList responseBody = response.getBody();
-
-        assertTrue(response.getStatusCode().equals(HttpStatus.OK));
-        assertTrue(responseBody.getTags().size() > 0);
     }
 
     @Test
     public void testGetExpensesBySearchCriteriaWithTagNamesSettedOnly() throws Exception {
-        // Creating new expenses
-        Expense testExpenseToAdd = testUtils.generateTestExpense(20, null);
-        HttpEntity<Expense> entity = new HttpEntity<Expense>(testExpenseToAdd, headers);
-        ResponseEntity<ExpensesList> generatedExpenseResponse = restTemplate.exchange(createURLWithPort("/expense"), HttpMethod.POST, entity, ExpensesList.class);
-        // Creating request url based on params
-        String tagName = generatedExpenseResponse.getBody().getExpenses().get(0).getTags().get(0).getName();
-        String requestURL = createURLWithPort("/expenses/criteria?tagNames=" + tagName + "&fromDate=&toDate=");
-        // Executing request
-        HttpEntity<String> getExpensesByCriteriaEntity = new HttpEntity<String>(null, headers);
-        ResponseEntity<ExpensesList> getExpensesByCriteriaResponse = restTemplate.exchange(requestURL, HttpMethod.GET, entity, ExpensesList.class);
-
-        assertTrue(getExpensesByCriteriaResponse.getStatusCode().equals(HttpStatus.OK));
-        assertTrue(getExpensesByCriteriaResponse.getBody().getExpenses().size() > 0);
+        fail("Not implemented yet.");
     }
 }
